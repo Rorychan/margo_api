387f58200ba285f26715f0d6c24d7399a6dc855d
Rorychan <rorychan0697@gmail.com>
2022-05-02 22:02:47 +0500

    added some functionality

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
+import uuid
 
+orders = {}
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Hello World'
 
+@app.route("/api/v1/orders", methods = ["POST"])
+def orders_processor():
+    global orders
+    if request.method == "POST":
+        if request.cookies.get("UserId") == None:
+            user_id = uuid.uuid4()
+            orders.update({str(user_id): {}})
+        else:
+            user_id = request.cookies.get("UserId")
+        user_id_str = str(user_id)
+        order_data = request.get_json()
+        print(order_data)
+
+        order_id = uuid.uuid4()
+        order_id_str = str(order_id)
+        print(order_id)
+        orders[user_id_str].update({order_id_str: order_data})
+        resp = make_response(jsonify(orders[user_id_str]),200)
+        resp.set_cookie("UserId",user_id_str)
+        print(orders)
+    return resp
+@app.route("/api/v1/orders", methods = ["GET"])
+def get_orders():
+    global orders
+    if request.method == "GET":
+        if request.cookies.get("UserId") == None:
+            resp = make_response({"Message": "User not found"}, 404)
+            return resp
+        else:
+            resp = make_response(orders[request.cookies.get("UserId")], 200)
+            return resp
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=5001, debug = True)
